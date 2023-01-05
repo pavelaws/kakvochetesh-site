a6eb01f7ddaf360d4bacf8ef2905033e2c86248f
Pavel Seraphimov <116275653+pavelaws@users.noreply.github.com>
2023-01-05 12:44:35 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Pavel Seraphimov';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'blog.pavel.se';
+    : 'Онлайн дневник';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
